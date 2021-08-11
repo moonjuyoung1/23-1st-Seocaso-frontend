3547518a924feacbcd89aa27246aab22ba7f67f4
cocacollllla <cocacollllla@gmail.com>
2021-08-11 16:45:54 +0900

    Modify : 테스트 컴포넌트 삭제, 리뷰 반영 코드 수정

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
-import SearchTest from "./components/Nav/SearchTest";
-import Product from "./pages/Product/Product";
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/Nav" component={Nav} />
-          <Route exact path="/SearchTest" component={SearchTest} />
-          <Route exact path="/Product" component={Product} />
+          <Route exact path="/nav" component={Nav} />
         </Switch>
       </Router>
     );
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import "./Nav.scss";
 
 class Nav extends React.Component {
@@ -9,8 +9,6 @@ class Nav extends React.Component {
     super();
     this.state = {
       searchInput: "",
-      popular: [],
-      showModal: false,
     };
   }
 
@@ -20,89 +18,52 @@ class Nav extends React.Component {
     });
   };
 
-  enterInsertValue = (e) => {
+  enterInsertValue = (e) => { 
     if (e.key === "Enter") {
       this.props.history.push({
-        pathname: "/SearchTest",
-        state: { test: this.state.searchInput },
+        pathname: "/Search",
+        state: { keyword: this.state.searchInput },
       });
     }
   };
 
-  handleModal = () => {
-    this.setState({
-      showModal: true,
-    });
-  };
-
-  handleModalRemove = () => {
-    this.setState({
-      showModal: false,
-    });
-  };
-
-  componentDidMount() {
-    fetch("/data/SearchResultData.json")
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({
-          popular: data,
-        });
-      });
-  }
-
   render() {
-    const { searchInput, showModal, popular } = this.state;
-    const { handleModal, handleChange, enterInsertValue, handleModalRemove } =
+    const { searchInput } = this.state;
+    const { handleChange, enterInsertValue } =
       this;
     return (
-      <div className="nav">
-        <nav>
-          <img alt="서카소 로고" src="../images/seocaso_logo.png" />
-          <div className="search">
-            <div className="search-box">
-              <FontAwesomeIcon icon={faSearch} className="search-icon" />
-              <input
-                type="text"
-                value={searchInput}
-                placeholder="카페 또는 주소를 검색해보세요."
-                onClick={handleModal}
-                onChange={handleChange}
-                onKeyPress={enterInsertValue}
-              />
-              <div
-                className="searchResultBox"
-                style={{ display: showModal && "block" }}
-              >
+      
+        <nav className="nav">
+          <div className="nav-wrapper">
+            <img alt="서카소 로고" src="../images/seocaso_logo.png" />
+            <div className="search">
+              <div className="search-box">
+                <FontAwesomeIcon icon={faSearch} className="search-icon" />
+                <input
+                  type="text"
+                  value={searchInput}
+                  placeholder="카페 또는 주소를 검색해보세요."
+                  onChange={handleChange}
+                  onKeyPress={enterInsertValue}
+                />
+              </div>
+              {/* merge후 로그인시 .login / 비로그인시 .not-login 보이도록 하기*/}
+              <div className="not-login">
+                <button>로그인</button>
+                <button>회원가입</button>
+              </div>
+              <div className="login">
+                <div>다슬님</div>
                 <div>
-                  <FontAwesomeIcon
-                    icon={faTimes}
-                    onClick={handleModalRemove}
-                    className="modal-close-icon"
-                  />
-                  <p>인기검색어</p>
-                  <div>
-                    {popular.map((el) => {
-                      return <div key={el.id}>{el.cafeName}</div>;
-                    })}
-                  </div>
+                  <img alt="프로필사진" src="../../images/profilephoto.png" />
                 </div>
               </div>
             </div>
-            {/* merge후 로그인시 .login / 비로그인시 .not-login 보이도록 하기*/}
-            <div className="not-login">
-              <button>로그인</button>
-              <button>회원가입</button>
-            </div>
-            <div className="login">
-              <div>다슬님</div>
-              <div>
-                <img alt="프로필사진" src="../../images/profilephoto.png" />
-              </div>
-            </div>
+
           </div>
+          
         </nav>
-      </div>
+     
     );
   }
 }
